d8518273ce483aeef5be65cd73f922f7a74cb702
rubyharkness <ruby.harkness@unchainedlabs.com>
2021-07-15 12:21:49 -0700

    commit 3

--- cslist.py
+++ cslist.py
@@ -33,17 +33,33 @@ for file in os.listdir(r'C:\SWDevelopment\ULActions'):
     #Print the properties Property = NameOfProperty
     text = f.read()
  
+# search_words = set(['class'])
+
+# for item in os.listdir(r'C:\SWDevelopment\ULActions'):
+#     f = open("C:\\SWDevelopment\\ULActions\\" + item, "r")
+#     sentences = f.readlines()
+#     for sentence in sentences: 
+#         words_in_sentence = set(sentence.split())
+#         if words_in_sentence.intersection(search_words): 
+#             words2 = sentence.split()
+#             print("The class is: {} ".format(words2[2]))
+            
+# search_words2 = set(['public'])
+
+# for item in os.listdir(r'C:\SWDevelopment\ULActions'):
+#     f = open("C:\\SWDevelopment\\ULActions\\" + item, "r")
+#     sentences = f.readlines()
+#     for sentence in sentences: 
+#         words_in_sentence = set(sentence.split())
+#         if words_in_sentence.intersection(search_words2): 
+#             words2 = sentence.split()
+#            # print words2
+#             if len(words2) == 3: 
+#                 print("The properties are: {} ".format(words2[2]))
+#     f.close()
+
 search_words = set(['class'])
 
-for item in os.listdir(r'C:\SWDevelopment\ULActions'):
-    f = open("C:\\SWDevelopment\\ULActions\\" + item, "r")
-    sentences = f.readlines()
-    for sentence in sentences: 
-        words_in_sentence = set(sentence.split())
-        if words_in_sentence.intersection(search_words): 
-            words2 = sentence.split()
-            print("The class is: {} ".format(words2[2]))
-            
 search_words2 = set(['public'])
 
 for item in os.listdir(r'C:\SWDevelopment\ULActions'):
@@ -51,10 +67,15 @@ for item in os.listdir(r'C:\SWDevelopment\ULActions'):
     sentences = f.readlines()
     for sentence in sentences: 
         words_in_sentence = set(sentence.split())
-        if words_in_sentence.intersection(search_words2): 
+        if words_in_sentence.intersection(search_words):
             words2 = sentence.split()
-           # print words2
+            print("The class is: {} ".format(words2[2]))
+        if words_in_sentence.intersection(search_words2):  
+            words2 = sentence.split()
             if len(words2) == 3: 
                 print("The properties are: {} ".format(words2[2]))
     f.close()
+
+
+
  
